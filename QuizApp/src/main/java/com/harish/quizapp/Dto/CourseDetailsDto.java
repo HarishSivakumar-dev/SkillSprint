f5504f3d8d7aa6c0009a0bfbe544a4cf6fcc6d68
Harish Sivakumar <harishss.2k07@gmail.com>
2025-12-28 16:12:56 +0530

    Optimization for N+1 Query prevention

--- QuizApp/src/main/java/com/harish/quizapp/Dto/CourseDetailsDto.java
+++ QuizApp/src/main/java/com/harish/quizapp/Dto/CourseDetailsDto.java
@@ -1,122 +1,95 @@
-package com.harish.quizapp.Model;
+package com.harish.quizapp.Dto;
 
 import java.time.LocalDateTime;
 
 import com.harish.quizapp.enums.CourseStatus;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name="CourseDetails")
-public class CourseDetails
+public class CourseDetailsDto
 {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int id;
-	
-	@ManyToOne(fetch=FetchType.EAGER)
-	@JoinColumn(name="instructor_id")
-	private UserRegistration instructor;
+	private int courseId;
+	private int instructorId;
+	private String userName;
+	private String fullName;
 	private String title;
 	private String description;
 	private String catagory;
 	private String level;
 	private String duration;
 	private LocalDateTime createdAt;
-	
-	@Enumerated(EnumType.STRING)
 	private CourseStatus status;
 	private float rating;
-	
-	public CourseStatus getStatus() {
-		return status;
+	public int getCourseId() {
+		return courseId;
 	}
-
-	public void setStatus(CourseStatus status) {
-		this.status = status;
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
 	}
-
-	public int getId() {
-		return id;
+	public int getInstructorId() {
+		return instructorId;
 	}
-
-	public void setId(int id) {
-		this.id = id;
+	public void setInstructorId(int instructorId) {
+		this.instructorId = instructorId;
 	}
-
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 	public String getCatagory() {
 		return catagory;
 	}
-
 	public void setCatagory(String catagory) {
 		this.catagory = catagory;
 	}
-
 	public String getLevel() {
 		return level;
 	}
-
 	public void setLevel(String level) {
 		this.level = level;
 	}
-
 	public String getDuration() {
 		return duration;
 	}
-
 	public void setDuration(String duration) {
 		this.duration = duration;
 	}
-	
 	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
-
 	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
-
-	public UserRegistration getInstructor() {
-		return instructor;
+	public CourseStatus getStatus() {
+		return status;
 	}
-
-	public void setInstructor(UserRegistration instructor) {
-		this.instructor = instructor;
+	public void setStatus(CourseStatus status) {
+		this.status = status;
 	}
-
 	public float getRating() {
 		return rating;
 	}
-
 	public void setRating(float rating) {
 		this.rating = rating;
 	}
 	
-	
-	
+
 }
