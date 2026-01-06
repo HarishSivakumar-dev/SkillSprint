3e22292e4385acb87f74390ee0031305f590b4b4
Harish Sivakumar <harishss.2k07@gmail.com>
2026-01-06 14:42:22 +0530

    change of column name in nativeQuery

--- QuizApp/src/main/java/com/harish/quizapp/DataRepos/InstStatRepo.java
+++ QuizApp/src/main/java/com/harish/quizapp/DataRepos/InstStatRepo.java
@@ -1,9 +1,24 @@
 package com.harish.quizapp.DataRepos;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.harish.quizapp.Model.InstructorStatUpdate;
 
 public interface InstStatRepo extends JpaRepository<InstructorStatUpdate, Integer>
 {
+	@Query(value="""
+			SELECT i.inst_id AS instId,
+			 i.event_type AS eventType, 
+			 SUM(i.delta_value) AS totChange
+			 FROM instructor_stat_update i
+			 WHERE proceeded=false
+			 GROUP BY i.inst_id, i.event_type
+			 """, nativeQuery=true)
+	List<InstructorStatUpdateProjection> findByRecordsForStat();
+	
+	
+	@Query(value="SELECT * FROM instructor_stat_update WHERE processed=false", nativeQuery=true)
+	List<InstructorStatUpdate> findallPending();
 
 }
