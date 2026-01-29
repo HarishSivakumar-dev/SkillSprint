6c6019fd4dedae63e864b1666e127bce3659ed91
Harish Sivakumar <harishss.2k07@gmail.com>
2026-01-29 22:27:20 +0530

    SupplyAsync definitions for completablefutures

--- QuizApp/src/main/java/com/harish/quizapp/DataRepos/AdminPromotionRepo.java
+++ QuizApp/src/main/java/com/harish/quizapp/DataRepos/AdminPromotionRepo.java
@@ -1,10 +1,12 @@
 package com.harish.quizapp.DataRepos;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.harish.quizapp.Model.AdminApplication;
 import com.harish.quizapp.enums.PromotionStatus;
 
@@ -16,5 +18,11 @@ public interface AdminPromotionRepo extends JpaRepository<AdminApplication, Inte
 	Optional<AdminApplication> findByUser_Id(int id);
 	
 	int countByPromotionStatus(PromotionStatus ps);
+	
+	@Query(value="SELECT COUNT(u) FROM AdminApplication u WHERE u.reviewedOn>= :start AND u.reviewedOn< :end AND u.adminManager.id=:id")
+	int countByWindow(@Param(value="start") LocalDateTime start,@Param(value="end") LocalDateTime end, @Param(value="id") int id);
+	
+	@Query(value="SELECT COUNT(u) FROM AdminApplication u WHERE u.appliedDate<= :today")
+	int countByOldApplications(@Param(value="today") LocalDateTime today);
 	
 }
--- QuizApp/src/main/java/com/harish/quizapp/asyncFunctionCalls/AdminManagerAsyncCalls.java
+++ QuizApp/src/main/java/com/harish/quizapp/asyncFunctionCalls/AdminManagerAsyncCalls.java
@@ -1,20 +1,65 @@
 package com.harish.quizapp.asyncFunctionCalls;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.context.SecurityContextHolder;
+import com.harish.quizapp.DataRepos.AdminLogsRepo;
 import com.harish.quizapp.DataRepos.AdminPromotionRepo;
+import com.harish.quizapp.DataRepos.UserRepo;
 import com.harish.quizapp.enums.PromotionStatus;
 
 public class AdminManagerAsyncCalls
 {
 	@Autowired
 	private AdminPromotionRepo apr;
+	@Autowired
+	private Executor exe;
+	@Autowired
+	private UserRepo ur;
+	@Autowired
+	private AdminLogsRepo alr;
 	
-	@Async
+	
 	public CompletableFuture<Integer> getAllAdmins()
 	{
-		return CompletableFuture.supplyAsync(()-> apr.countByPromotionStatus(PromotionStatus.Promoted), null);
+		return CompletableFuture.supplyAsync(()-> apr.countByPromotionStatus(PromotionStatus.Promoted), exe);
 	}
-
+	
+	public CompletableFuture<Integer> getAllAdminApplications()
+	{
+		return CompletableFuture.supplyAsync(()-> apr.countByPromotionStatus(PromotionStatus.Pending), exe);
+	}
+	
+	public CompletableFuture<Integer> getallApplicationsApprovedToday()
+	{
+		LocalDateTime yt= LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
+		LocalDateTime tm= LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).plusDays(1);
+		
+		String reg= SecurityContextHolder.getContext().getAuthentication().getName();
+		
+		
+		return CompletableFuture.supplyAsync(()-> 
+		{	
+			int id=ur.findByUserName(reg).get().getId();
+			return apr.countByWindow(yt, tm, id);
+			
+		}, exe);
+		
+	}
+	public CompletableFuture<Integer> getOldAdminApplications()
+	{
+		LocalDateTime td= LocalDateTime.now().minusDays(3);
+		
+		return CompletableFuture.supplyAsync(()-> apr.countByOldApplications(td), exe);
+	}
+	public CompletableFuture<Integer> getActiveAdminsToday()
+	{
+		LocalDate dat= LocalDate.now();
+		
+		return CompletableFuture.supplyAsync(()-> alr.countByActiveAdmins(dat));
+	}
+	
 }
