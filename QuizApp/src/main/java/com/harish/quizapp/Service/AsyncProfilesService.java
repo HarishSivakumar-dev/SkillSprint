69b5ad3c176bc4f95a8d40b2ec7870dd08543208
Harish Sivakumar <harishss.2k07@gmail.com>
2026-01-29 22:28:09 +0530

    Admin manager analytics computation via async implementation

--- QuizApp/src/main/java/com/harish/quizapp/Service/AsyncProfilesService.java
+++ QuizApp/src/main/java/com/harish/quizapp/Service/AsyncProfilesService.java
@@ -7,14 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.harish.quizapp.DataRepos.SuperAdminRepo;
+import com.harish.quizapp.Dto.AdminManagerAnalyticsDto;
 import com.harish.quizapp.Dto.SuperAdminAnalyticsDto;
 import com.harish.quizapp.Model.SuperAdminAnalytics;
+import com.harish.quizapp.asyncFunctionCalls.AdminManagerAsyncCalls;
 
 @Service
 public class AsyncProfilesService 
 {
 	@Autowired
 	private SuperAdminRepo sar;
+	@Autowired
+	private AdminManagerAsyncCalls calls;
 	
 	public ResponseEntity<SuperAdminAnalyticsDto> getSuperAdminAnalytics()
 	{
@@ -25,9 +29,29 @@ public class AsyncProfilesService
 		
 	}
 	
-	public CompletableFuture<?> getAdminManagerAnalytics()
+	public CompletableFuture<AdminManagerAnalyticsDto> getAdminManagerAnalytics()
 	{
-		return null;
+		CompletableFuture<Integer> adminno= calls.getAllAdmins();
+		CompletableFuture<Integer> actadminstod= calls.getActiveAdminsToday();
+		CompletableFuture<Integer> pending= calls.getOldAdminApplications();
+		CompletableFuture<Integer> approved= calls.getallApplicationsApprovedToday();
+		CompletableFuture<Integer> application= calls.getAllAdminApplications();
+		
+		return CompletableFuture.allOf(adminno,actadminstod,pending,approved,application)
+								.thenApply(v->
+											{
+												AdminManagerAnalyticsDto aad= new AdminManagerAnalyticsDto();
+												aad.setActiveAdminsToday(actadminstod.join());
+												aad.setApplicationsApprovedToday(approved.join());
+												aad.setTotAdmins(adminno.join());
+												aad.setPendingApplications(pending.join());
+												aad.setAllApplications(application.join());
+												
+												return aad;
+											
+											}
+												);
+		
 	}
 	
 	public CompletableFuture<?> getAdminAnalytics()
