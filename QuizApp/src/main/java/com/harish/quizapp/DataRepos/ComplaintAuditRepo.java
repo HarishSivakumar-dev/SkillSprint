c879d0fe8f29aae1854576b9e2576d793c19dfbb
Harish Sivakumar <harishss.2k07@gmail.com>
2026-01-31 13:34:14 +0530

    Audit of admin case handles with repos

--- QuizApp/src/main/java/com/harish/quizapp/DataRepos/ComplaintAuditRepo.java
+++ QuizApp/src/main/java/com/harish/quizapp/DataRepos/ComplaintAuditRepo.java
@@ -1,6 +1,10 @@
 package com.harish.quizapp.DataRepos;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.harish.quizapp.Model.ComplaintAuditTable;
@@ -9,4 +13,7 @@ import com.harish.quizapp.Model.ComplaintAuditTable;
 public interface ComplaintAuditRepo extends JpaRepository<ComplaintAuditTable, Integer>
 {
 	int countByAdmin_Id(int id);
+	
+	@Query("SELECT COUNT(u) FROM ComplaintAuditTable u WHERE time>= :timestart AND time< :timeend AND u.admin.id= :adminid")
+	int countByAdminTasksToday(@Param(value="timestart") LocalDateTime timeStart, @Param(value="timeend") LocalDateTime timeEnd, @Param(value="adminid") int id);
 }
--- QuizApp/src/main/java/com/harish/quizapp/DataRepos/ComplaintsRepo.java
+++ QuizApp/src/main/java/com/harish/quizapp/DataRepos/ComplaintsRepo.java
@@ -1,9 +1,11 @@
 package com.harish.quizapp.DataRepos;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.harish.quizapp.Model.ComplaintsTable;
 import com.harish.quizapp.Model.UserRegistration;
 import com.harish.quizapp.enums.ComplaintStatus;
@@ -14,4 +16,10 @@ public interface ComplaintsRepo extends JpaRepository<ComplaintsTable, Integer>
 	List<ComplaintsTable> findByUser(UserRegistration ur);
 
 	List<ComplaintsTable> findByStatus(ComplaintStatus cs);
+	
+	int countByCreatedAtAndStatus(LocalDate date, ComplaintStatus cs);
+	
+	@Query("SELECT COUNT(u) FROM ComplaintsTable u WHERE createdAt<= :date AND status= :cs")
+	int countByOldRecords(@Param(value="date") LocalDate date, @Param(value="cs") ComplaintStatus cs);
+	
 }
