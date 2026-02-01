80226e2fc8090c2fec1b2de919b07c2c1b09c26b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-01 21:34:57 +0530

    Commit 2

--- QuizApp/src/main/java/com/harish/quizapp/DataRepos/UserRepo.java
+++ QuizApp/src/main/java/com/harish/quizapp/DataRepos/UserRepo.java
@@ -8,17 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.harish.quizapp.Model.UserRegistration;
 
-
 @Repository
-public interface UserRepo extends JpaRepository<UserRegistration,Integer>
+public interface UserRepo extends JpaRepository<UserRegistration, Integer>
 {
 	Optional<UserRegistration> findByUserName(String name);
 	Optional<UserRegistration> findByEmail(String email);
 	
 	
-	@Query(value="SELECT COUNT(u) from UserRegistration u JOIN u.roles r WHERE r.rolename= :rolename ")
+	@Query(value="SELECT COUNT(u) from UserRegistration u JOIN u.roles r WHERE r.rolename= : rolename ")
 	int countByRoles(@Param(value="rolename") String role);
 	
 	@Query(value="SELECT COUNT(u) from UserRegistration u WHERE u.joinedDate >=:start AND u.joinedDate< :end")
 	int countByMonthlyRegistrations(@Param(value="start") LocalDateTime start, @Param(value="end") LocalDateTime end);
+
 }
