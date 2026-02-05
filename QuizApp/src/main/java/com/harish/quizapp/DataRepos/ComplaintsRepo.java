ae45196338449f2b3efc4385b55392ec9b54a00e
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-05 15:47:04 +0530

    logic optimization with constraints

--- QuizApp/src/main/java/com/harish/quizapp/DataRepos/ComplaintsRepo.java
+++ QuizApp/src/main/java/com/harish/quizapp/DataRepos/ComplaintsRepo.java
@@ -2,6 +2,8 @@ package com.harish.quizapp.DataRepos;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,5 +23,9 @@ public interface ComplaintsRepo extends JpaRepository<ComplaintsTable, Integer>
 	
 	@Query("SELECT COUNT(u) FROM ComplaintsTable u WHERE createdAt<= :date AND status= :cs")
 	int countByOldRecords(@Param(value="date") LocalDate date, @Param(value="cs") ComplaintStatus cs);
+	
+	List<ComplaintsTable> findByInstructor_IdAndStatus(int id, ComplaintStatus st);
+	
+	Optional<ComplaintsTable> findByUser_IdAndInstructor_IdAndCourse_IdAndStatus(int uid, int instid, int cid, ComplaintStatus cs);
 	
 }
--- QuizApp/src/main/java/com/harish/quizapp/Service/ComplaintService.java
+++ QuizApp/src/main/java/com/harish/quizapp/Service/ComplaintService.java
@@ -1,7 +1,11 @@
 package com.harish.quizapp.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +13,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import com.harish.quizapp.DataRepos.ComplaintsRepo;
 import com.harish.quizapp.DataRepos.CoursesRepo;
+import com.harish.quizapp.DataRepos.InstructorRepo;
 import com.harish.quizapp.DataRepos.UserRepo;
+import com.harish.quizapp.DataRepos.ViolationTableRepo;
+import com.harish.quizapp.Dto.ComplaintInstructorDto;
 import com.harish.quizapp.Dto.ComplaintsDto;
+import com.harish.quizapp.Dto.ViolationResultDto;
 import com.harish.quizapp.Model.ComplaintsTable;
 import com.harish.quizapp.Model.CourseDetails;
+import com.harish.quizapp.Model.InstructorProfile;
 import com.harish.quizapp.Model.UserRegistration;
+import com.harish.quizapp.Model.ViolationsTable;
 import com.harish.quizapp.enums.ComplaintStatus;
 
 @Service
@@ -25,12 +35,23 @@ public class ComplaintService
 	private CoursesRepo cr;
 	@Autowired
 	private ComplaintsRepo comrep;
+	@Autowired
+	private InstructorRepo ir;
+	@Autowired
+	private ViolationTableRepo rp;
 
 	public ResponseEntity<String> addUserReport(ComplaintsDto ct)
 	{
 		String user=SecurityContextHolder.getContext().getAuthentication().getName();
 		UserRegistration us= ur.findByUserName(user).orElseThrow();
 		CourseDetails cd=cr.findById(ct.getId()).orElseThrow();
+		Optional<ViolationsTable> vt=rp.findByInstructor_Id(cd.getInstructor().getId());
+		Optional<ComplaintsTable> cmp= comrep.findByUser_IdAndInstructor_IdAndCourse_IdAndStatus(us.getId(),cd.getInstructor().getId(),cd.getId(),ComplaintStatus.Pending);
+		
+		if((vt.isPresent() && vt.get().isViolated()) || (cmp.isPresent()))
+		{
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("INSTRUCTOR ALREADY UNDER VIOLATION ! / A COMPLAINT IS STILL PENDING !");
+		}
 	
 		ComplaintsTable com=new ComplaintsTable();
 		com.setComments(ct.getComments());
@@ -39,6 +60,7 @@ public class ComplaintService
 		com.setReason(ct.getReason());
 		com.setCreatedAt(LocalDate.now());
 		com.setStatus(ComplaintStatus.Pending);
+		com.setCourse(cd);
 		
 		comrep.save(com);
 		
@@ -51,5 +73,65 @@ public class ComplaintService
 		UserRegistration us= ur.findByUserName(name).orElseThrow();
 		
 		return ResponseEntity.status(HttpStatus.FOUND).body(comrep.findByUser(us));
+	}
+	
+	public ResponseEntity<List<ComplaintInstructorDto>> getComplaintsInstructor()
+	{
+		String name= SecurityContextHolder.getContext().getAuthentication().getName();
+		InstructorProfile ip= ir.findByUserName_UserName(name).orElseThrow(()-> new NoSuchElementException());
+		
+		List<ComplaintsTable> tb = comrep.findByInstructor_IdAndStatus(ip.getId(), ComplaintStatus.Pending);
+		List<ComplaintInstructorDto> dto= new ArrayList<>();
+		
+		if(tb.isEmpty())
+		{
+			return ResponseEntity.status(HttpStatus.OK).body(dto);
+		}
+		
+		dto= tb.stream()
+			   .map(r->{
+						ComplaintInstructorDto bl= new ComplaintInstructorDto();
+						bl.setComments(r.getComments());
+					    bl.setComplaintId(r.getId());
+						bl.setCreatedAt(r.getCreatedAt());
+						bl.setInstructorId(r.getInstructor().getId());
+						bl.setInstructorUsername(r.getInstructor().getUserName().getUserName());
+					    bl.setReason(r.getReason());
+						bl.setStatus(r.getStatus());
+												
+						return bl;	
+						})
+				.collect(Collectors.toList());
+		
+		return ResponseEntity.status(HttpStatus.FOUND).body(dto);
+		
+		
+	}
+	
+	public ResponseEntity<ViolationResultDto> getViolationOfMe()
+	{
+		String name=SecurityContextHolder.getContext().getAuthentication().getName();
+		int id= ir.findByUserName_UserName(name).get().getId();
+		
+		Optional<ViolationsTable> vt= rp.findByInstructor_Id(id);
+		ViolationResultDto dt = new ViolationResultDto();
+		
+		if(vt.isEmpty())
+		{
+			dt.setFinalViolationCount(0);
+			dt.setInitialViolationCount(0);
+			dt.setIsViolated(false);
+			
+			return ResponseEntity.status(HttpStatus.OK).body(dt);
+		}
+		else 
+		{
+			dt.setFinalViolationCount(vt.get().getFinalViolationCount());
+			dt.setInitialViolationCount(vt.get().getInitialViolationCount());
+			dt.setIsViolated(vt.get().isViolated());
+			dt.setDateOfViolation(vt.get().getDateOfViolation());
+			
+			return ResponseEntity.status(HttpStatus.OK).body(dt);
+		}
 	}
 }
