7caf94c488c5c811e655fb92a1d91b73e79e2b67
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-07 23:37:19 +0530

    DTO for materials

--- QuizApp/src/main/java/com/harish/quizapp/Dto/StudentsDTO.java
+++ QuizApp/src/main/java/com/harish/quizapp/Dto/StudentsDTO.java
@@ -1,21 +1,23 @@
-package com.harish.quizapp.Model;
+package com.harish.quizapp.Dto;
 
 public class StudentsDTO
 {
 	private int courseid;
-	private String name;
+	private String userName;
 	private String Email;
+	private int userId;
+	
 	public int getCourseid() {
 		return courseid;
 	}
 	public void setCourseid(int courseid) {
 		this.courseid = courseid;
 	}
-	public String getName() {
-		return name;
+	public String getUserName() {
+		return userName;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 	public String getEmail() {
 		return Email;
@@ -23,4 +25,11 @@ public class StudentsDTO
 	public void setEmail(String email) {
 		Email = email;
 	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
 }
---
+++ QuizApp/src/main/java/com/harish/quizapp/Dto/StudyMaterialDto.java
@@ -0,0 +1,30 @@
+package com.harish.quizapp.Dto;
+
+public class StudyMaterialDto 
+{
+	private int topicId;
+	private String typeOfMaterial;
+	private String Url;
+	
+	public int getTopicId() {
+		return topicId;
+	}
+	public void setTopicId(int topicId) {
+		this.topicId = topicId;
+	}
+	public String getTypeOfMaterial() {
+		return typeOfMaterial;
+	}
+	public void setTypeOfMaterial(String typeOfMaterial) {
+		this.typeOfMaterial = typeOfMaterial;
+	}
+	public String getUrl() {
+		return Url;
+	}
+	public void setUrl(String url) {
+		Url = url;
+	}
+	
+	
+
+}
