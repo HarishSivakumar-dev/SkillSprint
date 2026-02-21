62055749917bb8664a22ae423889d45c00755d02
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-21 15:29:08 +0530

    the JSON serializer is modified with javatime module (object mapper) for serialization of java 8 features like localtime

--- QuizApp/src/main/java/com/harish/quizapp/config/RedisConfig.java
+++ QuizApp/src/main/java/com/harish/quizapp/config/RedisConfig.java
@@ -4,20 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.harish.quizapp.Model.UserProfile;
 
 @Configuration
 public class RedisConfig
 {
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisconnectionfactory)
+	RedisTemplate<String, UserProfile> redisTemplate(RedisConnectionFactory redisconnectionfactory)
 	{
-		RedisTemplate<String, Object> rt= new RedisTemplate<String, Object>();
+		ObjectMapper om= new ObjectMapper();
+		om.registerModule(new JavaTimeModule());
+		
+		Jackson2JsonRedisSerializer<UserProfile> seri= new Jackson2JsonRedisSerializer<>(UserProfile.class);
+		seri.setObjectMapper(om);
+		
+		RedisTemplate<String, UserProfile> rt= new RedisTemplate<String, UserProfile>();
 		rt.setKeySerializer(RedisSerializer.string());
-		rt.setValueSerializer(RedisSerializer.json());
+		rt.setValueSerializer(seri);
 		rt.setConnectionFactory(redisconnectionfactory);
 		rt.setHashKeySerializer(RedisSerializer.string());
-		rt.setHashValueSerializer(RedisSerializer.json());
+		rt.setHashValueSerializer(seri);
 		
 		return rt;
 	}
