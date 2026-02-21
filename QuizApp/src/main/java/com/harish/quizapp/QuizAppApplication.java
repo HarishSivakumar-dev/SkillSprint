10e1982c037fe3614065911710824515ea5b7284
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-21 15:29:43 +0530

    main class folder change reversal

--- QuizApp/src/main/java/com/harish/quizapp/QuizAppApplication.java
+++ QuizApp/src/main/java/com/harish/quizapp/QuizAppApplication.java
@@ -1,5 +1,7 @@
 package com.harish.quizapp;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- QuizApp/src/main/java/com/harish/quizapp/ServletInitializer.java
+++ QuizApp/src/main/java/com/harish/quizapp/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.harish.quizapp;
 
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
