77f177d65ab22aa4c1a49eec47f0157cf11d617b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-22 23:28:13 +0530

    added another bean for handling caching of updated inst profile

--- QuizApp/src/main/java/com/harish/quizapp/config/RedisConfig.java
+++ QuizApp/src/main/java/com/harish/quizapp/config/RedisConfig.java
@@ -8,19 +8,20 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.harish.quizapp.Model.InstructorProfile;
 import com.harish.quizapp.Model.UserProfile;
+
 
 @Configuration
 public class RedisConfig
 {
-	@Bean
+	@Bean(value="redisTemplate")
 	RedisTemplate<String, UserProfile> redisTemplate(RedisConnectionFactory redisconnectionfactory)
 	{
 		ObjectMapper om= new ObjectMapper();
 		om.registerModule(new JavaTimeModule());
 		
-		Jackson2JsonRedisSerializer<UserProfile> seri= new Jackson2JsonRedisSerializer<>(UserProfile.class);
-		seri.setObjectMapper(om);
+		Jackson2JsonRedisSerializer<UserProfile> seri= new Jackson2JsonRedisSerializer<>(om,UserProfile.class);
 		
 		RedisTemplate<String, UserProfile> rt= new RedisTemplate<String, UserProfile>();
 		rt.setKeySerializer(RedisSerializer.string());
@@ -28,6 +29,24 @@ public class RedisConfig
 		rt.setConnectionFactory(redisconnectionfactory);
 		rt.setHashKeySerializer(RedisSerializer.string());
 		rt.setHashValueSerializer(seri);
+		
+		return rt;
+	}
+	
+	@Bean(value="Instructor_Template")
+	RedisTemplate<String, InstructorProfile> redisTemplateInstructor(RedisConnectionFactory rcf)
+	{
+		ObjectMapper om= new ObjectMapper();
+		om.registerModule(new JavaTimeModule());
+		
+		Jackson2JsonRedisSerializer<InstructorProfile> jj= new Jackson2JsonRedisSerializer<>(om,InstructorProfile.class);
+		
+		RedisTemplate<String, InstructorProfile> rt= new RedisTemplate<String, InstructorProfile>();
+		rt.setKeySerializer(RedisSerializer.string());
+		rt.setValueSerializer(jj);
+		rt.setHashKeySerializer(RedisSerializer.string());
+		rt.setHashValueSerializer(jj);
+		rt.setConnectionFactory(rcf);
 		
 		return rt;
 	}
