e0c367f204a4b12763d776e39f5d4b10834cebf2
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-22 23:28:51 +0530

    named the executor bean

--- QuizApp/src/main/java/com/harish/quizapp/config/AsyncConfig.java
+++ QuizApp/src/main/java/com/harish/quizapp/config/AsyncConfig.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AsyncConfig 
 {
 	
-	@Bean
+	@Bean(value="asyncExecutor")
 	public Executor asyncExecutor()
 	{
 		ThreadPoolTaskExecutor ex= new ThreadPoolTaskExecutor();
