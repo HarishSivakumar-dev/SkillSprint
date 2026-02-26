834a21c930fb2707f862e8a430359e2e75e1dd3d
Harish Sivakumar <harishss.2k07@gmail.com>
2026-02-26 21:48:42 +0530

    add method to retrieve leaderboard data as a map with scores from zset of redis

--- QuizApp/src/main/java/com/harish/quizapp/Service/DashBoardService.java
+++ QuizApp/src/main/java/com/harish/quizapp/Service/DashBoardService.java
@@ -1,9 +1,32 @@
 package com.harish.quizapp.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DashBoardService
 {
-
+	@Autowired
+	@Qualifier("Dashboard_Template")
+	private RedisTemplate<String, String> rt;
+	
+	public Map<String, Double> getall()
+	{
+		Set<TypedTuple<String>> dash=rt.opsForZSet().reverseRangeWithScores("LeaderBoard:overall", 0, -1);
+		Map<String, Double> map=new HashMap<>();
+		
+		for(TypedTuple<String> t:dash)
+		{
+			map.put(t.getValue(), t.getScore());
+		}
+		
+		return map;
+		
+	}
 }
